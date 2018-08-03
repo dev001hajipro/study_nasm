7dd54df9a7513711b51e7ef17f337c6c1bb2647d
dev001hajipro <dev001hajipro@gmail.com>
2018-08-03 16:35:41 +0900

    update assembly

--- factorial/factorial.s
+++ factorial/factorial.s
@@ -1,22 +1,65 @@
 	.file	"factorial.c"
 	.intel_syntax noprefix
+# GNU C11 (Debian 6.3.0-18+deb9u1) version 6.3.0 20170516 (x86_64-linux-gnu)
+#	compiled by GNU C version 6.3.0 20170516, GMP version 6.1.2, MPFR version 3.1.5, MPC version 1.0.3, isl version 0.15
+# GGC heuristics: --param ggc-min-expand=100 --param ggc-min-heapsize=131072
+# options passed:  -imultiarch x86_64-linux-gnu factorial.c -masm=intel
+# -mtune=generic -march=x86-64 -O1 -fverbose-asm
+# options enabled:  -fPIC -fPIE -faggressive-loop-optimizations
+# -fasynchronous-unwind-tables -fauto-inc-dec -fbranch-count-reg
+# -fchkp-check-incomplete-type -fchkp-check-read -fchkp-check-write
+# -fchkp-instrument-calls -fchkp-narrow-bounds -fchkp-optimize
+# -fchkp-store-bounds -fchkp-use-static-bounds
+# -fchkp-use-static-const-bounds -fchkp-use-wrappers
+# -fcombine-stack-adjustments -fcommon -fcompare-elim -fcprop-registers
+# -fdefer-pop -fdelete-null-pointer-checks -fdwarf2-cfi-asm
+# -fearly-inlining -feliminate-unused-debug-types -fforward-propagate
+# -ffunction-cse -fgcse-lm -fgnu-runtime -fgnu-unique
+# -fguess-branch-probability -fident -fif-conversion -fif-conversion2
+# -finline -finline-atomics -finline-functions-called-once -fipa-profile
+# -fipa-pure-const -fipa-reference -fira-hoist-pressure
+# -fira-share-save-slots -fira-share-spill-slots -fivopts
+# -fkeep-static-consts -fleading-underscore -flifetime-dse
+# -flto-odr-type-merging -fmath-errno -fmerge-constants
+# -fmerge-debug-strings -fmove-loop-invariants -fomit-frame-pointer
+# -fpeephole -fplt -fprefetch-loop-arrays -freg-struct-return
+# -freorder-blocks -fsched-critical-path-heuristic
+# -fsched-dep-count-heuristic -fsched-group-heuristic -fsched-interblock
+# -fsched-last-insn-heuristic -fsched-rank-heuristic -fsched-spec
+# -fsched-spec-insn-heuristic -fsched-stalled-insns-dep -fschedule-fusion
+# -fsemantic-interposition -fshow-column -fshrink-wrap -fsigned-zeros
+# -fsplit-ivs-in-unroller -fsplit-wide-types -fssa-backprop -fssa-phiopt
+# -fstdarg-opt -fstrict-volatile-bitfields -fsync-libcalls
+# -ftoplevel-reorder -ftrapping-math -ftree-bit-ccp -ftree-builtin-call-dce
+# -ftree-ccp -ftree-ch -ftree-coalesce-vars -ftree-copy-prop -ftree-cselim
+# -ftree-dce -ftree-dominator-opts -ftree-dse -ftree-forwprop -ftree-fre
+# -ftree-loop-if-convert -ftree-loop-im -ftree-loop-ivcanon
+# -ftree-loop-optimize -ftree-parallelize-loops= -ftree-phiprop -ftree-pta
+# -ftree-reassoc -ftree-scev-cprop -ftree-sink -ftree-slsr -ftree-sra
+# -ftree-ter -funit-at-a-time -funwind-tables -fverbose-asm
+# -fzero-initialized-in-bss -m128bit-long-double -m64 -m80387
+# -malign-stringops -mavx256-split-unaligned-load
+# -mavx256-split-unaligned-store -mfancy-math-387 -mfp-ret-in-387 -mfxsr
+# -mglibc -mieee-fp -mlong-double-80 -mmmx -mno-sse4 -mpush-args -mred-zone
+# -msse -msse2 -mstv -mtls-direct-seg-refs -mvzeroupper
+
 	.text
 	.globl	factorial
 	.type	factorial, @function
 factorial:
 .LFB11:
 	.cfi_startproc
-	mov	eax, 1
-	cmp	edi, 1
-	jle	.L8
-	push	rbx
+	mov	eax, 1	# <retval>,
+	cmp	edi, 1	# n,
+	jle	.L8	#,
+	push	rbx	#
 	.cfi_def_cfa_offset 16
 	.cfi_offset 3, -16
-	mov	ebx, edi
-	lea	edi, -1[rdi]
-	call	factorial
-	imul	eax, ebx
-	pop	rbx
+	mov	ebx, edi	# n, n
+	lea	edi, -1[rdi]	# tmp91,
+	call	factorial	#
+	imul	eax, ebx	# <retval>, n
+	pop	rbx	#
 	.cfi_restore 3
 	.cfi_def_cfa_offset 8
 .L8:
@@ -33,16 +76,16 @@ factorial:
 main:
 .LFB12:
 	.cfi_startproc
-	sub	rsp, 8
+	sub	rsp, 8	#,
 	.cfi_def_cfa_offset 16
-	mov	edi, 5
-	call	factorial
-	mov	esi, eax
-	lea	rdi, .LC0[rip]
-	mov	eax, 0
-	call	printf@PLT
-	mov	eax, 0
-	add	rsp, 8
+	mov	edi, 5	#,
+	call	factorial	#
+	mov	esi, eax	#, ret
+	lea	rdi, .LC0[rip]	#,
+	mov	eax, 0	#,
+	call	printf@PLT	#
+	mov	eax, 0	#,
+	add	rsp, 8	#,
 	.cfi_def_cfa_offset 8
 	ret
 	.cfi_endproc
